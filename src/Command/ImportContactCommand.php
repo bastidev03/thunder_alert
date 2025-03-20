93b26f06ef4de9257a19e67047e4dda5cc684a00
bastidev03 <sebastien.molaire@laposte.net>
2025-03-20 15:01:21 +0100

    Implémentation du parser CSV

--- src/Command/ImportContactCommand.php
+++ src/Command/ImportContactCommand.php
@@ -1,12 +1,17 @@
 <?php
 namespace App\Command;
 
+//Library classes
 use Symfony\Component\Console\Attribute\AsCommand;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Logger\ConsoleLogger;
+
+//Project classes
+use App\Lib\CsvReader;
+
 
 #[AsCommand(
     name: 'app:import-contact',
@@ -16,13 +21,24 @@ class ImportContactCommand extends Command{
 
     protected function execute(InputInterface $input, OutputInterface $output):int
     {
-        $file_contact_path = $input->getArgument('file_path');
+        $file_path = $input->getArgument('file_path');
 
-        $output->writeln("Importing contacts from : \"$file_contact_path\"");
+        //TODO : try/catch ?
+        $csv_reader = new CsvReader($file_path);
+
+        $output->writeln("Importing contacts from : \"$file_path\"");
+
+        $csv_data = $csv_reader->get_array();
+
+
+
+
+
+
+
         //$logger = new ConsoleLogger($output);
         //$logger->info('Hello world !');
 
-        //Utiliser php natif fgetcsv()
         //In case of success
         return Command::SUCCESS;
 
--- src/Lib/CsvReader.php
+++ src/Lib/CsvReader.php
@@ -1,6 +1,7 @@
 <?php
 namespace App\Lib;
 
+use FilesystemIterator;
 use Symfony\Component\Filesystem\Exception\IOExceptionInterface;
 use Symfony\Component\Filesystem\Filesystem;
 use Symfony\Component\Filesystem\Path;
@@ -9,8 +10,52 @@ class CsvReader
 {
     private $file_resource;
 
+    //Absolute path
+    private $file_path;
+
     public function __construct(string $file_path)
     {
-        
+        $file_system = new Filesystem();
+        if(!$file_system->isAbsolutePath($file_path))
+        {
+            $file_path = Path::makeAbsolute($file_path, __DIR__.DIRECTORY_SEPARATOR.'..'.DIRECTORY_SEPARATOR.'..');
+        }
+
+        if(!$file_system->exists($file_path)) {
+            throw new \Exception("File ($file_path) not found");
+        }
+
+        if(is_dir($file_path)) {
+            throw new \Exception("File ($file_path) is not a file");
+        }
+
+        $file_extension = Path::getExtension($file_path, true);
+        if($file_extension !== 'csv') {
+            throw new \Exception("File ($file_path) is not a csv file");
+        }
+
+        $this->file_path = $file_path;
+
+        $this->file_resource = fopen($file_path, 'r');
+        if($this->file_resource === false) {
+            throw new \Exception("Failed reading file ($file_path)");
+        }
+    }
+
+    public function __destruct()
+    {
+        if($this->file_resource) {
+            fclose($this->file_resource);
+        }
+    }
+
+    public function get_array()
+    {
+        $array = fgetcsv($this->file_resource, 1000, ";");
+        if($array === false) {
+            throw new \Exception("Fail reading ({$this->file_path}) CSV file");
+        }
+
+        return $array;
     }
 }
