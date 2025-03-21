6f50002fc8458e88ad5c51e93b51ec3f59dd834d
bastidev03 <sebastien.molaire@laposte.net>
2025-03-21 21:21:26 +0100

    Création du service SmsService

---
+++ src/Service/SmsService.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Service;
+
+use Symfony\Component\HttpKernel\Log\Logger;
+
+use App\Lib\FormatValidator;
+use App\Lib\Logger;
+
+class SmsService
+{
+    const LOG_FILE = 'sms_history.log';
+
+    private $phone_number;
+
+    private $message;
+
+    public function __construct(string $phone_number, string $message)
+    {
+        if(!FormatValidator::checkPhone($phone_number))
+        {
+            throw new \Exception("Invalid phone number format ($phone_number)");
+        }
+
+        $this->$phone_number = $phone_number;
+        $this->message = $message;
+    }
+
+    public function sendSms():void
+    {
+        $log_message = "
+            SMS SENT
+                tel : {$this->phone_number}
+
+                sending_date : ".(date('Y-m-d H:i:s'))."
+
+                content : $message
+        \n";
+          
+        $http_logger = new Logger();
+        $http_logger->log($log_message);
+        Logger::log($log_message, self::LOG_FILE);
+    }
+}
