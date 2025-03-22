3e9753f1b27799d4eac990b3c9cc8a0b812fc752
bastidev03 <sebastien.molaire@laposte.net>
2025-03-22 21:09:32 +0100

    Ajout du script de migration de la BDD

--- config/bundles.php
+++ config/bundles.php
@@ -2,4 +2,7 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
+    SWouters\SqlMigrationsBundle\SqlMigrationsBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Zenstruck\Messenger\Test\ZenstruckMessengerTestBundle::class => ['test' => true],
 ];
